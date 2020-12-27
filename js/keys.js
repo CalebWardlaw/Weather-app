6e43f5f948fd55cb7e392a7a72d2946089625725
Caleb Wardlaw <calebwardlaw@gmail.com>
2020-12-27 09:47:40 -0600

    added site specific tokens

---
+++ js/keys.js
@@ -0,0 +1,4 @@
+var mapboxToken = 'pk.eyJ1IjoiY2FsZWJ3YXJkbGF3IiwiYSI6ImNrajdiMmJyZTEzaTczM255aHhzMDQ5Z2EifQ.WiidcQZmNvnzwjcG6Ia_yg'
+
+var OPEN_WEATHER_APPID =  '075b1c78280ac22d0888f38a3df0abb6'
+
